a9477af623d60e0948426e3cc09b40056b2908b8
Eliezer López <contacto@eliezerlopez.com>
2020-12-26 20:29:46 +0100

    Código base para implementar métodos observadores

--- empleado.cpp
+++ empleado.cpp
@@ -3,8 +3,6 @@
  * Plataforma: LinkedIn Learning
  * Instructor: @EliezerLopez
  * */
-
-#include <iostream>
 #include <string>
 using namespace std;
 
@@ -12,26 +10,17 @@ class Empleado {
 
 public:
 
-    string nombre, primerApellido;
-    float sueldoAnual;
-    static const int numeroDePagas = 12;
-
     float calcularSueldoMensual();
     string nombreCompleto();
 
 private:
 
-    
+    string nombre, primerApellido;
+    float sueldoAnual;
+    static const int numeroDePagas = 12;
 };
 
 int main(){
-
-    Empleado *empleado1 = new Empleado();
-    empleado1->nombre = "Alex";
-    empleado1->primerApellido = "Sampedro";
-    empleado1->sueldoAnual = 80000.50;
-
-    cout << "El sueldo anual de " << empleado1->nombreCompleto() << " es " << empleado1->calcularSueldoMensual() << ". " << endl;
 
     return 0;
 }
