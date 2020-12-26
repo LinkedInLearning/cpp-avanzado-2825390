5b2448ed092d3208950a0941bce718ae2e5ea123
Eliezer López <contacto@eliezerlopez.com>
2020-12-26 19:52:26 +0100

    Código corregido

--- empleado.cpp
+++ empleado.cpp
@@ -12,24 +12,26 @@ class Empleado {
 
 public:
 
+    string nombre, primerApellido;
+    float sueldoAnual;
+    static const int numeroDePagas = 12;
+
     float calcularSueldoMensual();
     string nombreCompleto();
 
 private:
 
-    string nombre, primerApellido;
-    float sueldoAnual;
-    static const int numeroDePagas = 12;
+    
 };
 
 int main(){
-    
-    Empleado empleado1 = new Empleado();
-    empleado1.nombre = Alex;
-    empleado1.primerApellido = Sampedro;
-    empleado1.sueldoAnual = 80000,50;
 
-    cout << "El sueldo anual de " empleado1->nombreCompleto " es " empleado1->calcularSueldoMensual() ". " << endl;
+    Empleado *empleado1 = new Empleado();
+    empleado1->nombre = "Alex";
+    empleado1->primerApellido = "Sampedro";
+    empleado1->sueldoAnual = 80000.50;
+
+    cout << "El sueldo anual de " << empleado1->nombreCompleto() << " es " << empleado1->calcularSueldoMensual() << ". " << endl;
 
     return 0;
 }
