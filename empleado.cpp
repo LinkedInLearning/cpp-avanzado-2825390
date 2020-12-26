767da55ec1b74f4f0378059674cfa1ef3296578a
Eliezer López <contacto@eliezerlopez.com>
2020-12-26 17:29:25 +0100

    Métodos y atributos

--- empleado.cpp
+++ empleado.cpp
@@ -4,15 +4,32 @@
  * Instructor: @EliezerLopez
  * */
 
+#include <string>
+using namespace std;
+
 class Empleado {
 
 public:
 
+    float calcularSueldoMensual();
+    string nombreCompleto();
+
 private:
 
+    string nombre, primerApellido;
+    float sueldoAnual;
+    static const int numeroDePagas = 12;
 };
 
 int main(){
-
+    
     return 0;
+}
+
+float Empleado::calcularSueldoMensual(){
+    return (sueldoAnual/numeroDePagas);
+}
+
+string Empleado::nombreCompleto(){
+    return (nombre + " " + primerApellido);
 }
