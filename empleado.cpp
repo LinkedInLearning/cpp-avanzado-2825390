e823b505980dc360175745ba3a40cfdc4e0fdb0b
Eliezer López <contacto@eliezerlopez.com>
2020-12-28 20:38:10 +0100

    Generalización y especialización en C++

--- empleado.cpp
+++ empleado.cpp
@@ -7,7 +7,7 @@
 #include <iostream>
 using namespace std;
 
-class Empleado{
+class Empleado {
 public:
     Empleado(string _nombre, string _primerApellido) : nombre(_nombre), primerApellido(_primerApellido){};
     string nombreCompleto() const;
