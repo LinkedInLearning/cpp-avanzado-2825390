9f205fa291b13db51175c204a164e555030c5322
Eliezer López <contacto@eliezerlopez.com>
2020-12-28 20:12:48 +0100

    Código base para implementar herencia simple

--- empleado.cpp
+++ empleado.cpp
@@ -7,26 +7,20 @@
 #include <iostream>
 using namespace std;
 
-class Empleado{
+class Empleado {
 public:
-    Empleado(string _nombre, string _primerApellido);
-private:
-    string nombre, primerApellido;
-    float sueldoAnual;
-};
-
-class Supervisor {
-public:
-    Supervisor(string _nombre, string _primerApellido);
+    Empleado(string _nombre, string _primerApellido) : nombre(_nombre), primerApellido(_primerApellido){};
+    string nombreCompleto() const;
 private:
     string nombre, primerApellido;
     float sueldoAnual;
 };
 
 int main(){
-
-    /* Este código se utiliza sólo para realizar
-    una intrucción al concepto de herencia en C++. */
-
+    
     return 0;
 };
+
+string Empleado::nombreCompleto() const{
+    return (nombre + " " + primerApellido);
+}
