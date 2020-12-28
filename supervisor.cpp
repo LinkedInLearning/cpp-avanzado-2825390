3fde554e4bd7237dbaf6cce7a2bdaf42860e75b7
Eliezer López <contacto@eliezerlopez.com>
2020-12-28 16:15:06 +0100

    Código base para explicar las asociaciones entre clases

---
+++ supervisor.cpp
@@ -0,0 +1,45 @@
+/*
+ * Curso: C++ Avanzado
+ * Plataforma: LinkedIn Learning
+ * Instructor: @EliezerLopez
+ * */
+#include <string>
+#include <iostream>
+using namespace std;
+
+class Empleado;
+
+class Supervisor {
+public:
+    void supervisaA(Empleado& empleado);
+private:
+    string nombre, primerApellido;
+    Empleado* empleadoSupervisado;
+};
+
+class Empleado {
+public:
+    void esSupervisadoPor(Supervisor& supervisor);
+private:
+    string nombre, primerApellido;
+    Supervisor* supervisor;
+};
+
+int main(){
+    
+    Empleado empleado;
+    Supervisor supervisor;
+
+    empleado.esSupervisadoPor(supervisor);
+    supervisor.supervisaA(empleado);
+
+    return 0;
+};
+
+void Supervisor::supervisaA(Empleado& empleado){
+    empleadoSupervisado = &empleado;
+}
+
+void Empleado::esSupervisadoPor(Supervisor& supervisor){
+    this->supervisor = &supervisor;
+}
