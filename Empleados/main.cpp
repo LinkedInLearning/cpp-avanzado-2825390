27a5cfa4440267e3014751a20df7320afeabd4df
Eliezer López <contacto@eliezerlopez.com>
2020-12-29 15:59:11 +0100

    Estructura mínima de un proyecto en C++

--- Empleados/main.cpp
+++ Empleados/main.cpp
@@ -3,14 +3,15 @@
  * Plataforma: LinkedIn Learning
  * Instructor: @EliezerLopez
  * */
+
 #include <string>
 #include <iostream>
 using namespace std;
- 
+
 int main(){
 
     Empleado* p_empleado = new Empleado("Evan", "Craft");
     cout << p_empleado->nombreCompleto() << endl;
-
+    
     return 0;
 }
